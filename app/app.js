9d14af114ee74681794df329816408ffa52a3399
Myron Mavko <mmavko@gmail.com>
2019-10-23 16:25:14 +0300

    ui

---
+++ app/app.js
@@ -0,0 +1,58 @@
+const {Fragment} = React;
+
+import {setApplication} from "./actions.js";
+import {connect} from "./store.js";
+import ApplicationA from "./applicationA.js";
+
+const applications = {
+  "Application A": ApplicationA,
+  "Application B": null,
+  "Application C": null,
+};
+
+const App = ({currentApplication, dispatch}) => {
+  const CurrentApp = applications[currentApplication];
+
+  const statusBar = (
+    <div
+      style={{padding: 20, marginBottom: 20, borderBottom: "2px solid grey"}}
+    >
+      {CurrentApp ? (
+        <Fragment>
+          Running application "{currentApplication}"{" "}
+          <button onClick={() => dispatch(setApplication(null))}>Close</button>
+        </Fragment>
+      ) : (
+        "Please choose the application"
+      )}
+    </div>
+  );
+
+  const dashboard = Object.keys(applications).map(name => (
+    <div
+      key={name}
+      onClick={() => dispatch(setApplication(name))}
+      style={{
+        cursor: "pointer",
+        border: "2px solid orange",
+        padding: 10,
+        width: 100,
+        height: 100,
+        float: "left",
+        marginLeft: 20,
+        backgroundColor: "khaki",
+      }}
+    >
+      {name}
+    </div>
+  ));
+
+  return (
+    <Fragment>
+      {statusBar}
+      {CurrentApp ? <CurrentApp /> : dashboard}
+    </Fragment>
+  );
+};
+
+export default connect(App);
